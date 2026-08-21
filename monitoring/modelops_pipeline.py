0930ae141c0fe45514088399eac394d66859a9ff
nickgeorgemathew <148985027+nickgeorgemathew@users.noreply.github.com>
2026-08-21 10:33:28 +0530

    modelops functions added for model health and model metrics

--- monitoring/modelops_pipeline.py
+++ monitoring/modelops_pipeline.py
@@ -1,7 +1,7 @@
 from config.settings import BEST_MODEL_PATH,BEST_MODEL_NAME
 from monitoring.drift_detector import DriftDetector,run_drift_pipeline
 from monitoring.metrics_monitor import MetricsMonitor,run_monitor_pipeline
-from monitoring.alert_manager import AlertManager
+from monitoring.alert_manager import AlertManager,run_alert_manager
 
 
 
@@ -10,14 +10,25 @@ alert=AlertManager()
 drift=DriftDetector()
 
 class ModelopsPipeline:
-    
+
 
     @staticmethod
-    def model_status_condition():
-        "get the health,metric,drift of the model and return "
+    def model_metrics_drift():
+        "get the metrics and drift/residuals of the model and return performance_flag,metric_week,metric_24,drift_flag,feature_drift_flag,residual_drift_flag"
         performance_flag,metric_week,metric_24=run_monitor_pipeline()
         drift_flag,feature_drift_flag,residual_drift_flag=run_drift_pipeline()
+        return performance_flag,metric_week,metric_24,drift_flag,feature_drift_flag,residual_drift_flag
 
+    @staticmethod
+    def model_health(drift_flag,performance_flag):
+        "get the health of the model and return confition_flag and trigger_action"
+        condition_flag,trigger_action=run_alert_manager(drift_flag,performance_flag)
+        return condition_flag,trigger_action
+    
+
+
+
+    
         
 
        
